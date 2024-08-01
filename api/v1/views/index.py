3149e2bea27e8937dc3e21428aa56aeff5a070f8
root <you@example.com>
2024-08-01 15:58:01 -0700

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,3 +4,15 @@ from api.v1.views import app_views
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
