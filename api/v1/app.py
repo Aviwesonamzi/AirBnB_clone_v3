0d5658a50331ae022571fd33c4eea53bf45cf30f
root <you@example.com>
2024-08-01 16:03:26 -0700

    error

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,11 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 errors with a JSON response"""
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
